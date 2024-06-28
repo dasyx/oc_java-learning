d33673795051eca0c8df7b536f52a7a0d3423a04
dasyx <seanpnn00@gmail.com>
2024-06-28 22:43:23 +0200

    add while loop

--- src/Loop.java
+++ src/Loop.java
@@ -1,7 +1,22 @@
 public class Loop {
+//    public static void main(String[] args) {
+//        for(int i=0; i < 5; i++) {
+//            System.out.println("Clap your hands");
+//        }
+//    }
+//    // Enhanced for loop
+//    public static void main(String[] args) {
+//        int[] myArray = {4, 13, 22, 31};
+//        for (int j : myArray) {
+//            System.out.println(j);
+//        }
+//    }
     public static void main(String[] args) {
-        for(int i=0; i < 5; i++) {
-            System.out.println("Clap your hands");
+        int[] myArray = {4, 13, 25, 36, 47};
+        int sum = 0;
+        for(int i=0; i < myArray.length; i++){
+            sum += myArray[i];
         }
+        System.out.println(sum);
     }
 }
---
+++ src/WhileLoop.java
@@ -0,0 +1,27 @@
+public class WhileLoop {
+    // While loop
+//    public static void main(String[] args) {
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println("Clap your hands");
+//            i++;
+//        }
+//    }
+//    // Do-while loop
+//    public static void main(String[] args) {
+//        int i = 0;
+//        do {
+//            System.out.println("Clap your hands");
+//            i++;
+//        } while (i < 5);
+//    }
+//    // Break statement
+//    public static void main(String[] args) {
+//        for(int i=0; i < 5; i++) {
+//            if (i == 3) {
+//                break;
+//            }
+//            System.out.println("Clap your hands");
+//        }
+//    }
+}
