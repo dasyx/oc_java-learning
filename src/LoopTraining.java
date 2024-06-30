b75fd9c55bf831416fa9912840209bf921f38740
dasyx <seanpnn00@gmail.com>
2024-06-30 20:54:34 +0200

    add loop training + random number

--- src/LoopTraining.java
+++ src/LoopTraining.java
@@ -1,11 +1,11 @@
 public class LoopTraining {
     public static void main(String[] args) {
-        int[] myValues = {4, 7, 12, 25, 36};
+//        int[] myValues = {4, 7, 12, 25, 36};
 //        for (int i = 0; i < myValues.length; i++) {
 //            System.out.println(myValues[i]);
 //        }
-        while(i < myValues.length) {
-
-        }
+//        while(i < myValues.length) {
+//
+//        }
     }
 }
