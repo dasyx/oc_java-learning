f643a8dbbfbc83a70a36a90160ed511cf8d45548
dasyx <seanpnn00@gmail.com>
2024-07-04 00:42:53 +0200

    add object class

---
+++ src/ObjectClass.java
@@ -0,0 +1,34 @@
+public class ObjectClass {
+
+    public static void main(String[] args) {
+        //déclaration d'un nouvel objet de type Marque
+        Marque apple = new Marque("Apple");
+
+        Telephone iphone = new Telephone(34000, "Iphone", apple);
+
+        System.out.println(iphone);
+
+    }
+}
+
+    class Telephone {
+        int pixels;
+        String nom;
+        Marque marque;
+
+        //ceci est un constructor
+        public Telephone(int pixels, String nom, Marque marque) {
+            //ne pas oublier de mettre à jour la valeur des attributs par les arguments
+            this.pixels = pixels;
+            this.nom = nom;
+            this.marque = marque;
+        }
+    }
+
+    class Marque {
+        String nom;
+
+        public Marque(String nom) {
+            this.nom = nom;
+        }
+    }
