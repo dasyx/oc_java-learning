f293959588ad2dc6dc7b385d453d22871edef8a3
dasyx <seanpnn00@gmail.com>
2024-07-08 19:29:22 +0200

    add work on Stack

---
+++ src/Stack.java
@@ -0,0 +1,8 @@
+public class Stack {
+
+    public static void main(String[] args) {
+        // On commence par déclarer le type d'éléments que l'on va stocker
+        String[] tableau = new String[10];
+    }
+
+}
