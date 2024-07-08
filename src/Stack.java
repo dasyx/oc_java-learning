67d21350f58b687834a7fa2cbcada9c0cf144b38
dasyx <seanpnn00@gmail.com>
2024-07-08 19:59:25 +0200

    add tableau array in Stack class

--- src/Stack.java
+++ src/Stack.java
@@ -2,7 +2,11 @@ public class Stack {
 
     public static void main(String[] args) {
         // On commence par déclarer le type d'éléments que l'on va stocker
-        String[] tableau = new String[10];
+        String[] tableau = new String[10]; // On écrit le nb d'éléments que l'on souhaite stocker
+
+        for(int i = 0; i < tableau.length; i++){
+            System.out.println(tableau[i]);
+        }
     }
 
 }
