88859c9a98e372ad27ea70a70f377060b4a0b928
dasyx <seanpnn00@gmail.com>
2024-07-08 21:50:37 +0200

    work on Stack + add values

--- src/Stack.java
+++ src/Stack.java
@@ -4,9 +4,13 @@ public class Stack {
         // On commence par déclarer le type d'éléments que l'on va stocker
         String[] tableau = new String[10]; // On écrit le nb d'éléments que l'on souhaite stocker
 
-        for(int i = 0; i < tableau.length; i++){
+        // Ajoute une valeur à la variable "tableau" à la quatrième occurrence de la boucle
+        //tableau[3] = "Index 3";
+        //tableau[3] = "Une autre valeur"    // va écraser la valeur précédente dans le tableau au même index (position)
+
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = "Index " + i;
             System.out.println(tableau[i]);
         }
     }
-
 }
