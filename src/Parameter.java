8cc6cf904b985fe60bf9b0e0107d79d7e9bc008e
dasyx <seanpnn00@gmail.com>
2024-07-15 22:34:39 +0200

    add type valeur in Parameter class

--- src/Parameter.java
+++ src/Parameter.java
@@ -3,9 +3,21 @@ public class Parameter {
     public static void main(String[] args){
         int result = displayParameter(4, 8);
         System.out.println(result);
+
+        // Type valeur
+        // Déclarez la variable "a" en tant que "int" et l'initialiser avec une valeur de 10
+        int a = 10;
+        // Déclarez la variable "b" en tant que "int" et l'initialiser avec une copie de la valeur de "a"
+        int b = a;
+        System.out.println(a); // -> 10
+        System.out.println(b); // -> 10
+        a = 15;
+        System.out.println(a); // -> 15
+        System.out.println(b); // -> 10
     }
     public static int displayParameter(int length, int width){
         return 2*(length + width);
     }
+
 
 }
