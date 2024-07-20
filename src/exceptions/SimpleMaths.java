05f8caaba73a5b55b1b5b997a2ff5ebf41f73d71
dasyx <seanpnn00@gmail.com>
2024-07-20 09:51:55 +0200

    add commentaires in simple maths

--- src/exceptions/SimpleMaths.java
+++ src/exceptions/SimpleMaths.java
@@ -1,21 +1,26 @@
-package exceptions;
+package exceptions; // Déclaration du package 'exceptions'
 
-import java.util.List;
+import java.util.List; // Importation de la classe List de la bibliothèque Java
 
 public class SimpleMaths {
 
-    /** calculez la valeur moyenne d'une liste d'entiers
+    /**
+     * Calculez la valeur moyenne d'une liste d'entiers
      *
      * @param listOfIntegers une liste contenant des nombres entiers
      * @return la moyenne de la liste
      */
-
     public static int calculateAverage(List<Integer> listOfIntegers) {
-        int average=0;
-        for (int value: listOfIntegers) {
-            average+=value;
+        int average = 0; // Initialisation de la variable 'average' à 0
+
+        // Boucle à travers chaque entier dans la liste
+        for (int value : listOfIntegers) {
+            average += value; // Ajoute la valeur actuelle à 'average'
         }
-        average/=listOfIntegers.size();
-        return average;
+
+        // Divise la somme totale par la taille de la liste pour obtenir la moyenne
+        average /= listOfIntegers.size();
+
+        return average; // Retourne la moyenne calculée
     }
 }
