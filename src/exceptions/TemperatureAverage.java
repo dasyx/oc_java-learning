8d9b575b9df8ff20c8cf841b352382d32ea89e48
dasyx <seanpnn00@gmail.com>
2024-07-20 09:53:31 +0200

    add commentaires in temperature average

--- src/exceptions/TemperatureAverage.java
+++ src/exceptions/TemperatureAverage.java
@@ -1,32 +1,36 @@
-package exceptions;
+package exceptions; // Déclaration du package 'exceptions'
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; // Importation de la classe ArrayList de la bibliothèque Java
+import java.util.List; // Importation de la classe List de la bibliothèque Java
 
 public class TemperatureAverage {
 
-    /** affiche la température moyenne à partir des valeurs fournies comme arguments en ligne de commande
+    /**
+     * Affiche la température moyenne à partir des valeurs fournies comme arguments en ligne de commande
      *
      * @param args liste de températures séparées par des espaces
      */
     public static void main(String[] args) {
 
+        // Crée une nouvelle liste pour stocker les températures enregistrées en degrés Celsius
         List<Integer> recordedTemperaturesInDegreesCelcius = new ArrayList<>();
 
+        // Ajoute quelques températures fixes à la liste
         recordedTemperaturesInDegreesCelcius.add(8);
         recordedTemperaturesInDegreesCelcius.add(12);
         recordedTemperaturesInDegreesCelcius.add(15);
         recordedTemperaturesInDegreesCelcius.add(22);
 
-        // remplissez la liste à partir des valeurs fournies comme arguments en ligne de commande
+        // Remplissez la liste à partir des valeurs fournies comme arguments en ligne de commande
         for (String stringRepresentationOfTemperature : args) {
+            // Convertit chaque argument de chaîne en un entier (température)
             int temperature = Integer.parseInt(stringRepresentationOfTemperature);
+            // Ajoute la température convertie à la liste
             recordedTemperaturesInDegreesCelcius.add(temperature);
         }
 
-        // calculez et affichez la température moyenne
+        // Calculez et affichez la température moyenne
         int averageTemperature = SimpleMaths.calculateAverage(recordedTemperaturesInDegreesCelcius);
         System.out.println("The average temperature is " + averageTemperature);
     }
-
 }
