cf348c1e1dc2274f1648af872edf1670869dd6da
dasyx <seanpnn00@gmail.com>
2024-09-27 10:13:33 +0200

    add lambda function example

---
+++ src/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args){
+        int result = 10;
+        var result2 = 20;
+
+        var result3 = 4;
+
+        System.out.printf("Le contenu de mes variables %s %s %s%n", result, result2, result3);
+    }
+}
