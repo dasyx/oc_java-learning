fd0f53e091ef4e5ba8eb4974620c1631eafd46f3
dasyx <seanpnn00@gmail.com>
2024-09-30 10:58:12 +0200

    add types in java

---
+++ src/Types.java
@@ -0,0 +1,26 @@
+public class Types {
+
+    public static void main(String[] args){
+        //Les entiers
+        byte octet = 127;
+        short unPeuPlusGrand = 2314;
+        int entier = 332121;
+        long entierLong = 1313213513068561L; //on rajoute un grand L
+
+        //Les flottants
+        float petitFloat = 42.4f;   //on rajoute un petit f
+        double grandFloat = 16468.464;
+
+        //Les caractères
+        char uneLettre = 'f'; //on utilise les simples quotes
+        char echap = '\''; //on utilise l'entislash pour représenter un caractère
+
+        //Les booléens
+        boolean uneCondition = true;
+
+        //Les chaînes de caractères
+        String uneChaine = "qsdfqs";
+
+    }
+
+}
